e421f9e840ec7db239a4a3969f738cb2d11c51e9
MahMosTash <mahdayrmostasharh@gmail.com>
2023-12-05 05:52:40 +0330

    Second edit

--- calculator.c
+++ calculator.c
@@ -1,1 +1,22 @@
+  GNU nano 7.2                                                                                       calculator.c
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+int calculate(int a, int b) {
+// A different calculation
+return a + b;
+}
